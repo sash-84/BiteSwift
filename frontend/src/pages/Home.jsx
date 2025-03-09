77f8955244cc3e56b4e0daaff6c5fe43414d6a58
sash-84 <s.katale844@gmail.com>
2025-03-09 19:52:17 +0530

    modified Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,23 +1,15 @@
-import { useState } from "react";
-import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import FeaturedRestaurants from "../components/FeaturedRestaurants";
 import PopularDishes from "../components/PopularDishes";
 import CustomerReviews from "../components/CustomerReviews";
 import CTA from "../components/CTA";
 import Footer from "../components/Footer";
-import LoginModal from "./Login";
-import SignupModal from "./Signup";
 import Services from "../components/Services";
 
 const Home = () => {
 
-  const [isLoginOpen, setIsLoginOpen] = useState(false);
-  const [isSignupOpen, setIsSignupOpen] = useState(false);
-
   return (
     <div>
-      <Navbar openLogin={() => setIsLoginOpen(true)}/>
       <Hero />
       <FeaturedRestaurants />
       <PopularDishes />
@@ -25,28 +17,6 @@ const Home = () => {
       <CustomerReviews />
       <CTA />
       <Footer />
-
-      { /* Login Modal */}
-      {isLoginOpen && (
-        <LoginModal 
-          closeLogin={() => setIsLoginOpen(false)} 
-          openSignup={() => {
-            setIsLoginOpen(false);
-            setIsSignupOpen(true);
-          }}  
-        />
-      )}
-
-      {/* Signup Modal */}
-      {isSignupOpen && (
-        <SignupModal 
-          closeSignup={() => setIsSignupOpen(false)}
-          openLogin={() => {
-            setIsSignupOpen(false);
-            setIsLoginOpen(true);
-          }} 
-        />
-      )}
     </div>
   );
 };
