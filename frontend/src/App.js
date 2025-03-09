0292a38e7cc2637551df4491e8f0c38db7d5a95a
sash-84 <s.katale844@gmail.com>
2025-03-09 19:50:33 +0530

    modified App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,20 +3,52 @@ import {Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Cart from "./components/Cart";
 import About from "./components/About";
-import RestaurantPage from "./pages/RestaurantPage";
+import RestaurantsPage from "./pages/RestaurantsPage";
+import RestaurantDetails from "./components/RestaurantDetails";
 import ProfilePage from "./components/Profile";
+import Navbar from "./components/Navbar";
+import LoginModal from "./pages/Login";
+import SignupModal from "./pages/Signup";
 
 function App() {
-  const [user, setUser] = useState(null);  // Store user state
+  // const [user, setUser] = useState(null);
+  const [isLoginOpen, setIsLoginOpen] = useState(false);
+  const [isSignupOpen, setIsSignupOpen] = useState(false);
 
   return (
-      <Routes>
+    <>
+        <Navbar openLogin={() => setIsLoginOpen(true)}/>      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
-        <Route path="/restaurants" element={<RestaurantPage />} />
+        <Route path="/restaurants" element={<RestaurantsPage />} />
         <Route path="/about" element={<About />} />
         <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/restaurant/:id" element={<RestaurantDetails />} />
+
       </Routes>
+
+      { /* Login Modal */}
+      {isLoginOpen && (
+        <LoginModal 
+          closeLogin={() => setIsLoginOpen(false)} 
+          openSignup={() => {
+            setIsLoginOpen(false);
+            setIsSignupOpen(true);
+          }}  
+        />
+      )}
+
+      {/* Signup Modal */}
+      {isSignupOpen && (
+        <SignupModal 
+          closeSignup={() => setIsSignupOpen(false)}
+          openLogin={() => {
+            setIsSignupOpen(false);
+            setIsLoginOpen(true);
+          }} 
+        />
+      )}
+    </>
   );
 }
 
